d72228bdbdd96b509cb20c828243bc6bbc17c93e
Iryna <sagaidachnaaa@gmail.com>
2026-02-06 15:46:49 +0000

    updating script2

--- script2.js
+++ script2.js
@@ -1,7 +1,9 @@
 function msg() {
     document.write("You clicked the button! Congratulations!");
 }
-
+function color() {
+    document.body.style.backgroundColor = "lightblue";
+}
 function msg2() {
     document.write("Not that button!");
 }
